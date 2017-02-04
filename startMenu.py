2cc84431c14144c0c7f84da6777413caf12ea740
Whittenator <twhitten@mix.wvu.edu>
2017-02-04 23:49:22 +0000

    Lxterminal pops up when develop mode is clicked on

--- startMenu.py
+++ startMenu.py
@@ -2,10 +2,18 @@
 #simple GUI
 
 import tkinter
+import subprocess
+import os
 from tkinter import *
 
 #Create the window
 root = tkinter.Tk()
+
+def developCallBack():
+     subprocess.Popen("lxterminal")
+    
+   
+   
 
 #Modify root window
 root.title("Linux Tablet Project")
@@ -14,12 +22,14 @@ root.geometry("800x480")
 app = Frame(root,width=800,height=480)
 app.pack(side=TOP)
 
+
 #Linux Mode
 button1 = Button(app, text = "Linux Mode", fg="red")
 button1.grid(row=0,column=1,pady=70)
 
+
 #Develop Mode
-developBtn = Button(app, text = "Develop Mode", fg="green")
+developBtn = Button(app, text = "Develop Mode", fg="green", command = developCallBack)
 developBtn.grid(row=1,column=1,pady=70)
 
 #Play Mode
