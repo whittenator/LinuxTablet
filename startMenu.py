824e6bfc4f22a4e99acf0433ca02c0f5c699d01e
Whittenator <twhitten@mix.wvu.edu>
2017-02-05 00:57:59 +0000

    Added a couple commands that will make it to where the window can't be closed

--- startMenu.py
+++ startMenu.py
@@ -5,6 +5,7 @@ import tkinter
 import subprocess
 import time
 from tkinter import *
+from tkinter import messagebox
 
 #Create the window
 root = tkinter.Tk()
@@ -16,7 +17,9 @@ playPhoto = PhotoImage(file="playPic.png")
 
 def developCallBack():
      subprocess.Popen(['lxterminal','--working-directory=/usr/local/bin'])
-     
+
+def on_closing():
+     messagebox.askokcancel("Quit", "YOU MUST PICK A MODE!")
    
    
 
@@ -24,6 +27,11 @@ def developCallBack():
 root.title("Linux Tablet Project")
 root.geometry("800x480")
 root.resizable(False, False)
+#Command below takes away toolbar at top all together(BECAREFUL!)
+#root.overrideredirect(1)
+
+#Uncomment line below to make it to where the user can't leave
+#root.protocol("WM_DELETE_WINDOW", on_closing)
 
 app = Frame(root,width=800,height=480)
 app.pack(side=TOP)
