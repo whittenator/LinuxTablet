b1baffd7cd28ee4a6b0bd465e03ee2ed797bc119
Whittenator <twhitten@mix.wvu.edu>
2017-02-05 00:35:13 +0000

    Added some graphics to the buttons

--- startMenu.py
+++ startMenu.py
@@ -3,38 +3,44 @@
 
 import tkinter
 import subprocess
-import os
+import time
 from tkinter import *
 
 #Create the window
 root = tkinter.Tk()
 
+#Button Images
+devPhoto = PhotoImage(file="devPic.png")
+linPhoto = PhotoImage(file="linPic.png")
+playPhoto = PhotoImage(file="playPic.png")
+
 def developCallBack():
      subprocess.Popen(['lxterminal','--working-directory=/usr/local/bin'])
-    
+     
    
    
 
 #Modify root window
 root.title("Linux Tablet Project")
 root.geometry("800x480")
+root.resizable(False, False)
 
 app = Frame(root,width=800,height=480)
 app.pack(side=TOP)
 
 
 #Linux Mode
-button1 = Button(app, text = "Linux Mode", fg="red")
-button1.grid(row=0,column=1,pady=70)
+button1 = Button(app, text = "Linux Mode", fg="red", image=linPhoto)
+button1.grid(row=0,column=1,pady=60)
 
 
 #Develop Mode
-developBtn = Button(app, text = "Develop Mode", fg="green", command = developCallBack)
-developBtn.grid(row=1,column=1,pady=70)
+developBtn = Button(app, text = "Develop Mode", image=devPhoto, fg="green", command = developCallBack)
+developBtn.grid(row=1,column=1,pady=60)
 
 #Play Mode
-playBtn = Button(app, text = "Play Mode", fg="orange")
-playBtn.grid(row=2,column=1,pady=70)
+playBtn = Button(app, text = "Play Mode", fg="orange", image=playPhoto)
+playBtn.grid(row=2,column=1,pady=60)
 
 
 #Kick off the event loop
