f3236738ddd5a60d2cecc93d2fb8fca13d86774f
whittenator <twhitten@mix.wvu.edu>
2017-04-06 22:00:04 -0400

    Merged both updates into one file

--- startMenu.py
+++ startMenu.py
@@ -24,8 +24,8 @@ helpPhoto = PhotoImage(file="helpPic.png")
 #Runs the i7 application - requires the 'Linux_all' variant of the Inform 7 installation 
 def developCallBack():
 	#subprocess.Popen("i7") 
-	subprocess.Popen(['lxterminal',"--geometry=126x36",'--working-directory=/usr/local/bin/'])
-
+	subprocess.Popen(['lxterminal',"--geometry=98x30",'--working-directory=/usr/local/bin'])
+ 
 #Runs the Gargoyle interpreter - requires the 'gargoyle-free' package installed
 def playCallBack():
 	subprocess.Popen("gargoyle-free")
@@ -40,16 +40,36 @@ def helpCallBack():
                 #modify help box window
 	top = Toplevel()
 	top.title("Help Menu")
-	top.geometry("500x440")
+	top.geometry("400x240")
 	top.resizable(False, False)
 	#puts scrollbar into window
 	scrollbar = Scrollbar(top)
 	scrollbar.pack(side=RIGHT, fill=Y)
 	#inserts textbox into window
-	textbox = Text(top, height=4, width=50, wrap=WORD, font='roboto')
+	textbox = Text(top, height=4, width=50, wrap=WORD)
 	textbox.pack(side=LEFT, fill=BOTH)
 	scrollbar.config(command=textbox.yview)
 	textbox.config(yscrollcommand=scrollbar.set)
+	textbox.insert(END, """Linux Tablet Help Menu
+                                                1. Linux Mode:
+                                                In this mode you can explore Linux OS, Raspbian
+                                                2. Develop Mode:
+                                                After clicking on Develop Mode, you will be prompted with a terminal. Simply type i7 and the Inform 7 terminal editor will launch
+                                                3. Play Mode:
+                                                After clicking on Play Mode, Gargoyle for Inform 7 will launch. You will be brought to the folder where Inform 7 games are stored. Double click on the game you want to play.
+                                                FAQ:
+                                                Q-How do I import new games?\nA-\n1.Open Linux mode\n2. Navigate to the folder where Inform 7 games are stored\n3. Drag files into this folder from a USB drive or downloaded files from the web
+                                                Q-Where can I find Infrom 7 games online?\nA-You can download games from Inform 7 website or search for games online in any search engine
+                                                Q-Where can I learn more about the Inform 7 programming language?\nA-check out the documentation on Inform 7's website at: www.inform7.com
+                                                Q-What are the Inform 7 keywords to use when I am in a game?\nA-There are many different keywords in Inform 7 to use to interact with the game. In order to see which keywords your game uses, type 'help' when in a game. To see all keywords used by Inform 7, go to www.Inform7.com
+                                                Q-How do I develop Inform 7 games in something other than the terminal?\nA-As of now, the terminal editor is the only way to develop Inform 7 games in Raspbian.
+                                                Linux Tablet Project Information:
+                                                Developers: Anthony Stewart, Dale Marcot, and Travis Whitten\nVersion: 3.0\nAdvisor: Professor Van Scoy""")
+
+
+
+
+
 	textbox.insert(END, """                                     Linux Tablet Help Menu
 
 1. Linux Mode:
@@ -80,11 +100,11 @@ Developers: Anthony Stewart, Dale Marcot, and Travis Whitten
 Version: 3.0
 Advisor: Professor Van Scoy
 github: https://github.com/whittenator/LinuxTablet.git""")
-
-
+        
+        
 #Modify root window
 root.title("Linux Tablet Project")
-root.geometry("1024x600")
+root.geometry("800x480")
 root.config(bg="black")
 root.attributes('-fullscreen',True)
 root.resizable(False, False)
@@ -115,7 +135,7 @@ playBtn.grid(row=2,column=1,pady=30)
 
 #Help Button
 helpBtn = Button(root, text = "help", bg="black", bd=0,activebackground="black" ,highlightthickness = 0, image=helpPhoto, command = helpCallBack, cursor="question_arrow")
-helpBtn.place(x=900,y=500)
+helpBtn.place(x=750,y=430)
 #helpBtn.grid(row=2,column=3,padx=20)
  
  
