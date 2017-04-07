2934b47df9b0fc622f5ccbec91060786d08300e7
whittenator <twhitten@mix.wvu.edu>
2017-04-07 16:22:47 -0400

    Proofread help text, adjusted default window positions

--- startMenu.py
+++ startMenu.py
@@ -24,11 +24,11 @@ helpPhoto = PhotoImage(file="helpPic.png")
 #Runs the i7 application - requires the 'Linux_all' variant of the Inform 7 installation 
 def developCallBack():
 	#subprocess.Popen("i7") 
-	subprocess.Popen(['lxterminal',"--geometry=98x30",'--working-directory=/usr/local/bin'])
+	subprocess.Popen(['lxterminal',"--geometry=98x30+0+0",'--working-directory=/usr/local/bin'])
  
 #Runs the Gargoyle interpreter - requires the 'gargoyle-free' package installed
 def playCallBack():
-	subprocess.Popen("gargoyle-free")
+	subprocess.Popen('gargoyle-free')
 
 def on_closing():
      messagebox.askokcancel("Quit", "YOU MUST PICK A MODE!")
@@ -40,7 +40,7 @@ def helpCallBack():
                 #modify help box window
 	top = Toplevel()
 	top.title("Help Menu")
-	top.geometry("400x240")
+	top.geometry("400x240+200+100")
 	top.resizable(False, False)
 	#puts scrollbar into window
 	scrollbar = Scrollbar(top)
@@ -52,55 +52,19 @@ def helpCallBack():
 	textbox.config(yscrollcommand=scrollbar.set)
 	textbox.insert(END, """Linux Tablet Help Menu
                                                 1. Linux Mode:
-                                                In this mode you can explore Linux OS, Raspbian
+                                                In this mode, you can explore a full Linux OS using Raspbian.
                                                 2. Develop Mode:
-                                                After clicking on Develop Mode, you will be prompted with a terminal. Simply type i7 and the Inform 7 terminal editor will launch
+                                                After clicking on Develop Mode, you will be prompted with a terminal. Simply type 'i7' and the Inform 7 terminal editor will launch.
                                                 3. Play Mode:
                                                 After clicking on Play Mode, Gargoyle for Inform 7 will launch. You will be brought to the folder where Inform 7 games are stored. Double click on the game you want to play.
                                                 FAQ:
-                                                Q-How do I import new games?\nA-\n1.Open Linux mode\n2. Navigate to the folder where Inform 7 games are stored\n3. Drag files into this folder from a USB drive or downloaded files from the web
-                                                Q-Where can I find Infrom 7 games online?\nA-You can download games from Inform 7 website or search for games online in any search engine
+                                                Q-How do I import new games?\nA-\n1.Open Linux mode\n2. Navigate to the folder where Inform 7 games are stored\n3. Drag files into this folder from a USB drive or downloaded files from the web.
+                                                Q-Where can I find Inform 7 games online?\nA-You can download games from Inform 7 website or search for games online in any search engine
                                                 Q-Where can I learn more about the Inform 7 programming language?\nA-check out the documentation on Inform 7's website at: www.inform7.com
                                                 Q-What are the Inform 7 keywords to use when I am in a game?\nA-There are many different keywords in Inform 7 to use to interact with the game. In order to see which keywords your game uses, type 'help' when in a game. To see all keywords used by Inform 7, go to www.Inform7.com
                                                 Q-How do I develop Inform 7 games in something other than the terminal?\nA-As of now, the terminal editor is the only way to develop Inform 7 games in Raspbian.
                                                 Linux Tablet Project Information:
                                                 Developers: Anthony Stewart, Dale Marcot, and Travis Whitten\nVersion: 3.0\nAdvisor: Professor Van Scoy""")
-
-
-
-
-
-	textbox.insert(END, """                                     Linux Tablet Help Menu
-
-1. Linux Mode:
-In this mode you can explore Linux OS, Raspbian
-
-2. Develop Mode:
-After clicking on Develop Mode, you will be prompted with a terminal. Simply type i7 and the Inform 7 terminal editor will launch
-
-3. Play Mode:
-After clicking on Play Mode, Gargoyle for Inform 7 will launch. Locate the folder named Inform7_Games. Double click on the game you want to play.
-
-FAQ:
-
-Q-How do I import new games?\nA-\n1.Open Linux mode\n2. Navigate to the folder where Inform 7 games are stored\n3. Drag files into this folder from a USB drive or downloaded files from the web
-
-Q-Where can I find Infrom 7 games online?\nA-You can download games from Inform 7 website or search for games online in any search engine
-
-Q-Where can I learn more about the Inform 7 programming language?\nA-Check out the documentation on Inform 7's website at: www.inform7.com/learn/
-
-Q-What are the Inform 7 keywords to use when I am in a game?\nA-There are many different keywords in Inform 7 to use to interact with the game. In order to see which keywords your game uses, type 'help' when in a game. To see all keywords used by Inform 7, go to www.Inform7.com
-
-Q-How do I develop Inform 7 games in something other than the terminal?\nA-As of now, the terminal editor is the only way to develop Inform 7 games in Raspbian.
-
-Q-When in Linux Mode, how do I get the start menu to pop up again?\nA-You can either restart the tablet OR find the file named startMenu.py and execute it.
-
-Linux Tablet Project Information:
-Developers: Anthony Stewart, Dale Marcot, and Travis Whitten
-Version: 3.0
-Advisor: Professor Van Scoy
-github: https://github.com/whittenator/LinuxTablet.git""")
-        
         
 #Modify root window
 root.title("Linux Tablet Project")
